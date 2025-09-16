694c07e8145e262814e777cffb3798ed317ba88f
Arthur Mariano <arthvm@proton.me>
2025-09-16 10:48:56 -0300

    feat(auth): add Firebase authentication service
    Introduce user authentication powered by Firebase.
    
    An `AuthService` class is created to encapsulate all authentication
    logic, including sign-up, sign-in, and sign-out. The Firebase SDK
    is now configured at app launch.
    
    Co-authored-by: Guilherme Maggiorini <maggiorini.gui@gmail.com>
    Co-authored-by: Ian Braga <ian007953@gmail.com>

---
+++ Todolist/AuthService.swift
@@ -0,0 +1,55 @@
+//
+//  AuthService.swift
+//  Todolist
+//
+//  Created by Arthur Mariano on 16/09/25.
+//
+
+import Foundation
+import FirebaseAuth
+
+class AuthService: ObservableObject {
+    @Published var user: User? = nil
+    @Published var isSignedIn: Bool = false
+    
+    init() {
+        self.user = Auth.auth().currentUser
+        self.isSignedIn = user != nil
+    }
+    
+    func signUp(email: String, password: String, completion: @escaping (Error?) -> Void) {
+        Auth.auth().createUser(withEmail: email, password: password) {  result, error in
+            if let error = error {
+                print("Sign Up Error: \(error.localizedDescription)")
+                completion(error)
+                return
+            }
+
+            self.user = Auth.auth().currentUser
+            self.isSignedIn = self?.user != nil
+            completion(nil)
+        }
+    }
+    
+    func signIn(email: String, password: String, completion: @escaping (Error?) -> Void) {
+        Auth.auth().signIn(withEmail: email, password: password) { result, error in
+            if let error = error {
+                print("Sign In Error: \(error.localizedDescription)")
+            }
+            
+            self.user = Auth.auth().currentUser
+            self.isSignedIn = true
+            completion(nil)
+        }
+    }
+    
+    func signOut() {
+        do {
+            try Auth.auth().signOut()
+            self.user = nil
+            self.isSignedIn = false
+        } catch {
+            print("Sign Out Error: \(error.localizedDescription)")
+        }
+    }
+}
--- Todolist/TodolistApp.swift
+++ Todolist/TodolistApp.swift
@@ -6,9 +6,14 @@
 //
 
 import SwiftUI
+import FIrebase
 
 @main
 struct TodolistApp: App {
+    init() {
+        FirebaseApp.configue()
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
