1ddf48b2f4b8670cb195468edfd19568965b2579
Arthur Mariano <arthvm@proton.me>
2025-09-16 09:02:13 -0300

    Initial Commit

---
+++ Todolist/TodolistApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodolistApp.swift
+//  Todolist
+//
+//  Created by Arthur Mariano on 16/09/25.
+//
+
+import SwiftUI
+
+@main
+struct TodolistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
